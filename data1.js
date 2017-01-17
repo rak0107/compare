78e650ef46347cbb554af23c712effbb8343eef7
rak0107 <rakshith0107@gmail.com>
2017-01-17 18:54:35 +0530

    compare
    compare

---
+++ data1.js
@@ -0,0 +1,67 @@
+var data1 = {
+"objectInfo":{
+	"name" : "level0",
+	"displayLevel" : "1" 
+},
+"objectAssembly": [
+	{
+		"name" : "level1-obj1",
+		"id" : "1",
+		"displayValue" : "level1-obj1",
+		"attr1" : "value1",
+		"attr2" : "value2",
+		"attr3" : "value3"
+	},
+	{
+		"name" : "level1-obj2",
+		"id" : "2",
+		"displayValue" : "level1-obj2",
+		"attr1" : "value1",
+		"attr2" : "value2",
+		"attr3" : "value3",
+		"objectAssembly":[
+			{
+				"name" : "level2-obj1",
+				"id" : "2",
+				"displayValue" : "level2-obj1",
+			},
+			{
+				"name" : "level2-obj2",
+				"id" : "2",
+				"displayValue" : "level2-obj2",
+			},
+			{
+				"name" : "level2-obj3",
+				"id" : "2",
+				"displayValue" : "level2-obj3",
+			}
+		]
+	},
+	{
+		"name" : "level1-obj3",
+		"id" : "3",
+		"displayValue" : "level1-obj3",
+		"attr1" : "value1",
+		"attr2" : "value2",
+		"attr3" : "value3"
+		
+	},
+	{
+		"name" : "level1-obj4",
+		"id" : "4",
+		"displayValue" : "level1-obj4",
+		"attr1" : "value1",
+		"attr2" : "value2",
+		"attr3" : "value3"
+		
+	},
+	{	"name" : "level1-obj5",
+		"id" : "5",
+		"displayValue" : "level1-obj5",
+		"attr1" : "value1",
+		"attr2" : "value2",
+		"attr3" : "value3"
+		
+	}
+]
+};
---
+++ scripts/index.js
@@ -0,0 +1,60 @@
+/*var h = $('#handle'),
+    l = $('#left'),
+    r = $('#right'),
+    w = $('body').width() - 18;
+
+var isDragging = false;
+
+h.mousedown(function(e){
+    isDragging = true;
+    e.preventDefault();
+});
+$(document).mouseup(function(){
+    isDragging = false;
+}).mousemove(function(e){
+    if(isDragging){
+        l.css('width', e.pageX);
+        r.css('width', w - e.pageX);
+    }
+});*/ //commented code to support resizable divs
+var assembly1 = {};
+
+
+$(document).ready(function(){
+	assembly1 = data1; //data1 is coming from data1.js file.
+	$("#left").Tree(data1);
+    $("#right").Tree(data1);
+});
+
+/*Tree component*/
+$.fn.Tree = function(data){
+	console.log(this);
+	var uiRenderer = this;
+	
+	var assembly = data.objectAssembly;
+	var root = data.objectInfo;
+	
+	var $table = $("<table>",{id:"leftTable",class:"compareTable"});
+	var $firstRow = $("<th>",{id:"leftRoot",class:"root"}).append("<td>").text(root.name);
+	$table.append($firstRow);
+	generateTreeHTML(assembly,$table,0,false);
+	uiRenderer.append($table);
+}
+
+function generateTreeHTML(assembly,$parentElement,level,childNode){
+	level++;
+	$.each(assembly, function(key,value){
+		//alert(value.id);
+        var $img = $("<img>",{class:"icons",src:"images/1.jpg"});
+        if(childNode!=true)
+		var $tr = $("<tr>",{id:value.id,class:"treeItem","level":level}).append("<td>").text(value.displayValue).prepend($img);
+        else
+        var $tr = $("<tr>",{id:value.id,class:"treeItem childNode","level":level}).append("<td>").text(value.displayValue).prepend($img);
+		$parentElement.append($tr);
+		if(value.objectAssembly === undefined)
+			return;
+		else
+			generateTreeHTML(value.objectAssembly,$parentElement,level,true);
+		
+	});
+}
